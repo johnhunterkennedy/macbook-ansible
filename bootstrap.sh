fcad62b430fe9b1e85881149432ee76cf4750c27
John-Hunter Kennedy <hunter@John-Hunters-MacBook-Pro.local>
2018-05-10 15:22:57 +0200

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Script to bootstrap setting up a new MacBook with Ansible
+
+echo "*******************************************"
+echo    "Setting up your MacBook with Magic"
+echo "*******************************************"
+
+sudo easy_install pip
+sudo easy_install ansible
+
+installdir="/tmp/ansiblemac-$RANDOM"
+mkdir $installdir
+
+git clone  $installdir # repo URL
+if [ ! -d $installdir ]; then
+    echo "Failed to find ansiblemac."
+    echo "git cloned failed"
+    exit 1
+else
+    cd $installdir
+    ansible-playbook -i ./hosts playbook.yml --verbose
+fi
+
+echo "Completing setup..."
+
+rm -Rfv /tmp/$installdir
+
+echo "All done! Enjoy!"
+
+exit 0
