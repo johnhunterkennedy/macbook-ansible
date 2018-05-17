abda222af4159c3589659044607b32bff21a8349
John-Hunter Kennedy <hunter@John-Hunters-MacBook-Pro.local>
2018-05-17 12:27:20 +0200

    minor changes

---
+++ provision.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "*******************************************"
+echo    "Setting up your MacBook with Magic"
+echo "*******************************************"
+
+sudo easy_install pip
+sudo easy_install ansible
+
+installdir="/tmp/ansiblemac-$RANDOM"
+mkdir $installdir
+
+git clone https://github.com/johnhunterkennedy/macbook-ansible.git $installdir # repo URL
+if [ ! -d $installdir ]; then
+    echo "Failed to find ansiblemac."
+    echo "git cloned failed"
+    exit 1
+else
+    cd $installdir
+    ansible-playbook -i ./hosts playbook.yml --verbose
+fi
+
+echo "Completing setup..."
+
+rm -Rfv /tmp/$installdir
+
+echo "All done! Enjoy!"
+
+exit 0
