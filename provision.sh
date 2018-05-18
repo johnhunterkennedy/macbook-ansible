9c24a9faac91e63a87eb5791da52c20d4dc3eb6f
John-Hunter Kennedy <hunter@John-Hunters-MacBook-Pro.local>
2018-05-18 15:22:05 +0200

    edits

--- provision.sh
+++ provision.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "************************************"
-echo "Provisioning your MacBook with Magic"
-echo "************************************"
+echo "*******************"
+echo "Configuring MacBook"
+echo "*******************"
 
 sudo easy_install pip
 sudo easy_install ansible
